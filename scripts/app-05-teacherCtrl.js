01fc58c7c1acc60e0fe0b0afd08b0c99f06afbec
Ashtlind <Ashtlind@gmail.com>
2016-06-24 09:03:26 +1000

    Teacher dashboard functioning / Updating hue light bases on active student understanding percentages, all realtime

--- scripts/app-05-teacherCtrl.js
+++ scripts/app-05-teacherCtrl.js
@@ -1,4 +1,4 @@
-angular.module('IC').controller('Teacher', ['$scope', '$firebaseObject', '$firebaseArray', '$routeParams', '$rootScope','hue', 'fbRef', function ($scope, $firebaseObject, $firebaseArray, $routeParams, $rootScope, hue, fbRef) {
+angular.module('IC').controller('Teacher', ['$scope', '$firebaseObject', '$firebaseArray', '$routeParams', '$rootScope','hue', 'fbRef', '$interval', '$timeout', function ($scope, $firebaseObject, $firebaseArray, $routeParams, $rootScope, hue, fbRef, $interval, $timeout) {
     var root = fbRef;
 
     $scope.classid = $routeParams.classid.substring(1);
@@ -15,9 +15,6 @@ angular.module('IC').controller('Teacher', ['$scope', '$firebaseObject', '$fireb
 
     myHue.getLights().then(function(lights) {
       $scope.lights = lights;
-
-      console.log(lights);
-
       // Switch light 1 on
       myHue.setLightState(1, {"on": true}).then(function(response) {
         //$scope.lights[1].state.on = false;
@@ -70,9 +67,74 @@ angular.module('IC').controller('Teacher', ['$scope', '$firebaseObject', '$fireb
       } else {
         // Init some controller globals
         $scope.userData = $firebaseObject(root.child("Users").child(guid).child("User"));
+
+        // Load the color profile for hue
+        $scope.colorsLive = $firebaseObject(root.child("Users").child(guid).child("Colors"));
+        $scope.colorsLive.$loaded(function () {
+          console.log($scope.colorsLive);
+          if ($scope.colorsLive.$value == null) {
+            // Set the defaults for the hue colors
+            $scope.colorsLive.$value = [{"color":"Violet", "perc":10},
+              {"color":"RoyalBlue", "perc":50},
+              {"color":"Green", "perc":20},
+              {"color":"Aqua", "perc":20}];
+            $scope.colorsLive.$save();
+          }
+        });
+        $scope.colorsLive.$watch(function (event) {
+          // On changes to the live color - Syncronise to the local array
+          var tempList = new Array();
+          $scope.colorsLiveLength = 0;
+          console.log("top");
+          angular.forEach($scope.colorsLive, function (color, key) {
+            $scope.colorsLiveLength += 1;
+            if ($scope.colors[key] != undefined && ($scope.colors[key].color != color.color || $scope.colors[key].perc != color.perc)) {
+              console.log("Change detect", color, $scope.colors[key]);
+              $scope.colors[key].color = color.color;
+              $scope.colors[key].perc = color.perc;
+            }
+            tempList.push(angular.extend({}, color));
+          });
+          if ($scope.colors.length != $scope.colorsLiveLength) {
+            $scope.colors = tempList;
+          }
+          $scope.findFlexBasis();
+        });
+
         $scope.classPub = $firebaseObject(classRef.child("/Pub"));
       }
     });
+
+    // Check the student list every 30 seconds to see if there are any students that are not active
+    var activeStudentsIntervalPromise = $interval(function () {
+      $scope.updateActiveStudents();
+    }, 30000);
+    $scope.$on('$destroy', function () { $interval.cancel(activeStudentsIntervalPromise); });
+
+    // Updated on $watch even from firebase object > students
+    $scope.updateActiveStudents = function () {
+      if ($scope.Students != undefined && $scope.Students.List != undefined) {
+        var totalStudents = 0;
+        angular.forEach($scope.Students.List, function(student) {
+          // If student client has checked in in the last ~1.15 minutes - add them to the current participating students
+          var activeIndex = $scope.Students.ActiveList.indexOf(student);
+          if (student.$value >= Date.now() - 70000) {
+            totalStudents += 1;
+            // Check if the student is in the active array - if not add them
+            if (activeIndex < 0) {
+              $scope.Students.ActiveList.push(student);
+            }
+          } else {
+            // Check if the student is in the active array - if they are remove them
+            if (activeIndex >= 0) {
+              $scope.Students.ActiveList.splice(activeIndex, 1);
+            }
+          }
+        });
+        $scope.Students.StudentTotal = totalStudents;
+        $scope.updateTopicAnswers();
+      }
+    };
 
     // Get class info and setup the basic structure
     $scope.$watch('classPub', function (newVal, oldVal){
@@ -80,18 +142,10 @@ angular.module('IC').controller('Teacher', ['$scope', '$firebaseObject', '$fireb
         // If any info is changed or loaded in the class/Pub dir refresh the other watched objects related to it
         if (oldVal == undefined || (newVal.CurrentLesson != oldVal.CurrentLesson)) {
           // Update the lesson and students once the lesson has changed
+          $scope.Students.ActiveList = new Array();
           $scope.Students.List = $firebaseArray(classRef.child("Lessons").child(newVal.CurrentLesson).child("Students"));
           $scope.Students.List.$watch(function (event) {
-            if ($scope.Students != undefined && $scope.Students.List != undefined) {
-              var totalStudents = 0;
-              angular.forEach($scope.Students.List, function(student) {
-                // If student client has checked in in the last ~1.15 minutes - add them to the current participating students
-                if (student.$value >= Date.now() - 70000) {
-                  totalStudents += 1;
-                }
-              });
-              $scope.Students.StudentTotal = totalStudents;
-            }
+            $scope.updateActiveStudents();
           });
         }
         if (oldVal == undefined || (newVal.CurrentTopic != oldVal.CurrentTopic && newVal.CurrentTopic != undefined)) {
@@ -108,31 +162,45 @@ angular.module('IC').controller('Teacher', ['$scope', '$firebaseObject', '$fireb
       }
     });
 
-    // Handle the answer calculations
-    $scope.$watch('Topic.Answers', function (newVal, oldVal) {
-      if (newVal != undefined) {
+    // Handle the answer calculations - triggered by topic answers watch event
+    $scope.updateTopicAnswers = function () {
+      if ($scope.Answers != undefined) {
         $scope.Answers.Total = 0;
         $scope.Answers.zero = 0;
         $scope.Answers.one = 0;
         $scope.Answers.two = 0;
-        angular.forEach(newVal, function (Answer){
-          $scope.Answers.Total += 1;
-          if (Answer.Answer < 1) {
-            $scope.Answers.zero += 1;
-          } else if (Answer.Answer < 2) {
-            $scope.Answers.one += 1;
-          } else if (Answer.Answer < 3) {
-            $scope.Answers.two += 1;
-          }
-        });
-        console.log($scope.Answers);
+        // Loop through the active students - and see if they have an answer
+        if ($scope.Topic.Answers != undefined) {
+          angular.forEach($scope.Students.ActiveList, function (student) {
+            if ($scope.Topic.Answers[student.$id] != undefined) {
+              $scope.Answers.Total += 1;
+              if ($scope.Topic.Answers[student.$id].Answer < 1) {
+                $scope.Answers.zero += 1;
+              } else if ($scope.Topic.Answers[student.$id].Answer < 2) {
+                $scope.Answers.one += 1;
+              } else if ($scope.Topic.Answers[student.$id].Answer < 3) {
+                $scope.Answers.two += 1;
+              }
+            }
+          });
+        }
+        // Calculate the total percentage for the hue light to be based off
+        if ($scope.Students.StudentTotal > 0) {
+          $scope.Answers.Perc = 100 / ($scope.Students.StudentTotal / $scope.Answers.two);
+        } else {
+          $scope.Answers.Perc = 0;
+        }
+        $scope.findColor();
       } else {
-        $scope.Answers = {};
+        $scope.Answers = {Perc:0};
       }
+    };
+    $scope.$watch('Topic.Answers', function (newVal, oldVal) {
+      $scope.updateTopicAnswers();
     });
 
-    $scope.pluralsAreGoodIGuess = function (numb){
-      if(numb>1)
+    $scope.pluralsAreGoodIGuess = function (numb) {
+      if(numb>1 || numb==0)
         return 's'
     }
 
@@ -150,7 +218,6 @@ angular.module('IC').controller('Teacher', ['$scope', '$firebaseObject', '$fireb
 
     });
     $scope.createNewLesson = function() {
-
         // Create lesson with a new session
         var newLessonRef = new Firebase($scope.fbRefClass + "/Lessons");
 
@@ -205,11 +272,29 @@ angular.module('IC').controller('Teacher', ['$scope', '$firebaseObject', '$fireb
     $scope.avalicolors = ["RoyalBlue","Aqua", "Green","Yellow","Orange","Red","Fuchsia","Violet","Lavender","Transparent"];
     $scope.removeColor = "REMOVE";
 
-    //$scope.colors.push({"perc":"0%"}); // Util for start - to specify starting index
-    $scope.colors.push({"color":"Violet", "perc":"10"});
-    $scope.colors.push({"color":"RoyalBlue", "perc":"50"});
-    $scope.colors.push({"color":"Green", "perc":"20"});
-    $scope.colors.push({"color":"Aqua", "perc":"20"});
+    $scope.$watch('colors', function (newVal, oldVal) {
+      if (newVal != undefined) {
+        // Update colorsLive to save the changes to firebase
+        angular.forEach(newVal, function (color, key) {
+          if ($scope.colorsLive[key] == undefined || $scope.colorsLive[key].color != color.color || $scope.colorsLive[key].perc != color.perc) {
+            $scope.colorsLive[key] = color;
+            $scope.colorsLive.$save();
+          }
+        });
+        // Go through and trim down the colorsLive array
+        var clearAfter = newVal.length;
+        angular.forEach($scope.colorsLive, function (color, key) {
+          if (key >= clearAfter) {
+            $firebaseObject($scope.colorsLive.$ref().child(key)).$remove();
+          }
+        });
+        // Wait 0.6 seconds to allow for the transition before changing the color - as it uses comnputed styles
+        $timeout(function () {
+          $scope.findColor();
+        }, 600);
+      }
+    }, true);
+
 
     $scope.draggingColor = false;
     $scope.draggingColorStart = function () {
@@ -254,6 +339,7 @@ angular.module('IC').controller('Teacher', ['$scope', '$firebaseObject', '$fireb
             $scope.colors.splice(key, 1);
           }
         });
+        $scope.findFlexBasis();
       }
 
       if ($scope.colorbaradd != {}) {
@@ -264,50 +350,60 @@ angular.module('IC').controller('Teacher', ['$scope', '$firebaseObject', '$fireb
           $scope.colors.push({"color": $scope.colorbaradd.right});
         };
         $scope.colorbaradd = {};
+        $scope.findFlexBasis();
       }
     };
 
     // Recalculates percentages for the colors - triggered on mouse up for resizing and on adding / remove
     $scope.recalcColor = function () {
-      var containerWidth = $('.colorbar').width();
-      var tot = 0;
-      angular.forEach($scope.colors, function (color, key){
-        var wth = ($('#color-' + key).outerWidth(true) / containerWidth) * 100;
-        tot += wth;
-        color.perc = wth;
+      $(document).ready(function () {
+        var containerWidth = $('.colorbar').width();
+        var tot = 0;
+        angular.forEach($scope.colors, function (color, key){
+          var wth = ($('#color-' + key).outerWidth(true) / containerWidth) * 100;
+          tot += wth;
+          color.perc = wth;
+        });
       });
     };
 
     // Get the current color based on the percentage - update light
-    $scope.findColor = function (perc) {
-      var ret = "bgc-gray";
-      var lastperc = 0;
-      angular.forEach($scope.colors, function(col, key) {
-        if (lastperc <= perc && lastperc+col.perc > perc) {
-          console.log("found one!");
-          ret = "bglc-" + col.color;
-          var color = $("#color-" + key).css("color");
-          console.log(color);
-          var matchColors = /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/;
-          var match = matchColors.exec(color);
-          if (match !== null) {
-              console.log('Red: ' + match[1] + ' Green: ' + match[2] + ' Blue: ' + match[3]);
-              $scope.testthethings(match[1],match[2],match[3]);
+    $scope.findColor = function () {
+      $(document).ready(function () {
+        var ret = "bgc-gray";
+        var lastperc = 0;
+        angular.forEach($scope.colors, function(col, key) {
+          if (lastperc <= $scope.Answers.Perc && lastperc+col.perc > $scope.Answers.Perc) {
+            console.log("found one!");
+            ret = "bglc-" + col.color;
+            var color = $("#color-" + key).css("color");
+            console.log(color);
+            var matchColors = /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/;
+            var match = matchColors.exec(color);
+            if (match !== null) {
+                console.log('Red: ' + match[1] + ' Green: ' + match[2] + ' Blue: ' + match[3]);
+                $scope.testthethings(match[1],match[2],match[3]);
+            }
           }
-        }
-        lastperc += col.perc;
+          lastperc += col.perc;
+        });
+        return ret;
       });
-      return ret;
     };
 
     // Change the defined percentages for the colors into px based flex basises
     // Update on window width change and on data load from firebase
     $scope.findFlexBasis = function () {
-      var computedmargin = $("#color-0").outerWidth() - $("#color-0").width();
-      angular.forEach($scope.colors, function(col, key) {
-        var containerWidth = $('.colorbar').width();
-        var colorbasis = (containerWidth * (col.perc / 100)) - computedmargin;
-        $("#color-" + key).css("flexBasis", colorbasis);
+      $(document).ready(function () {
+        var computedmargin = $("#color-0").outerWidth() - $("#color-0").width();
+        angular.forEach($scope.colors, function(col, key) {
+          // Apply flex basis to all except the last section
+          if (key < $scope.colors.length - 1) {
+            var containerWidth = $('.colorbar').width();
+            var colorbasis = (containerWidth * (col.perc / 100)) - computedmargin;
+            $("#color-" + key).css("flexBasis", colorbasis);
+          }
+        });
       });
     };
 }]);
